a5a27c1158e25b30d53d2caa481eaa5ec3df890f
Nakul Sabharwal <nasabhar@microsoft.com>
2018-08-10 10:54:45 +0530

    first commit

---
+++ src/main/java/DemoClient.java
@@ -0,0 +1,24 @@
+
+public class DemoClient {
+	int a;
+	float b;
+	String name;
+	
+	public DemoClient() {
+		this.a = 1;
+		this.b = (float)0.9;
+		this.name = "Test_name";
+	}
+	
+	public int getA() {
+		return this.a;
+	}
+	
+	public float getB() {
+		return this.b;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
